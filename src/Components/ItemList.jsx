49f3cd156037f66adaa84a4fb109000efd315382
MariaGarciaDinsmann <laura.mldg@gmail.com>
2021-11-14 00:57:24 -0300

    agrego correcciones

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Item from './Item.jsx'
-import Cards from '../mocks/cards.jsx'
+import getData from '../mocks/cards.jsx'
 
 export default function ItemList() {
 
     const [cardList, setCardList] = useState([]);
     
     
-    const getData = () => {
-        return new Promise((resolve, reject) => {
-            if (!Cards.length) {
-                reject(new Error('No existen items'));
-            }
-            setTimeout(() => {
-                resolve(Cards);
-            }, 2000);
-        });
-    }
-
     async function fetchingData() {
         try {
             const data = await getData(); //no sigue la ejecucion del bloque del try hasta que no se complete el resolve de la promise
@@ -30,7 +19,9 @@ export default function ItemList() {
         }
     }
 
-    fetchingData();
+    useEffect(() => {        
+        fetchingData();
+    }, [])
 
     return (
         <Container>
@@ -41,10 +32,10 @@ export default function ItemList() {
                     '& > :not(style)': {
                         m: 1,
                         width: 270,
-                        height: 410,
+                        height: 410,                        
                     },
                 }}>
-                {cardList.map((card) => { return <Item id={card.id} descripcion={card.descripcion} precio={card.precio} alt={card.alt} imagen={card.imagen} initial={1} stock={card.stock}/> })}
+                {cardList.map((card) => { return <Item key={card.id} descripcion={card.descripcion} precio={card.precio} alt={card.alt} imagen={card.imagen} initial={1} stock={card.stock}/> })}
             </Box>
         </Container >
     )
--- src/mocks/cards.jsx
+++ src/mocks/cards.jsx
@@ -1,36 +1,14 @@
-const Cards = [
-    {
-        id: 1,
-        descripcion: "Zapatilla Topper hombre Strong Pace Negro/lima Sring",
-        precio: "$6.399",
-        alt: "Zapatillas Topper",
-        imagen: 0,
-        stock: 5
-    },
-    {
-        id: 2,
-        descripcion: "Descripción 2",
-        precio: "$7.664",
-        alt: "Zapatillas",
-        imagen: 1,
-        stock: 10
-    },
-    {
-        id: 3,
-        descripcion: "Descripción 3",
-        precio: "$10.999",
-        alt: "Zapatillas",
-        imagen: 2,
-        stock: 7
-    },
-    {
-        id: 4,
-        descripcion: "Descripción 4",
-        precio: "$4.677",
-        alt: "Zapatillas",
-        imagen: 3,
-        stock: 3
-    }
-];
+import Cards from '../catalogo/catalogo.json'
 
-export default Cards;
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        if (!Cards.length) {
+            reject(new Error('No existen items'));
+        }
+        setTimeout(() => {
+            resolve(Cards);
+        }, 2000);
+    });
+}
+
+export default getData;
