94212700b7358007e5e7c944235b892bf8733bb4
MariaGarciaDinsmann <laura.mldg@gmail.com>
2021-11-25 11:32:54 -0300

    Subo routing con modificaciones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import { createMuiTheme } from '@mui/material';
@@ -22,28 +22,18 @@ const theme = createMuiTheme({
 
 function App() {
 
-  const [users, setUsers] = useState([])
-  const [currentUser, setCurrentUser] = useState({})
   const [currentProduct, setCurrentProduct] = useState({})
-
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=20')
-      .then(res => res.json())
-      .then(data => setUsers(data.results))
-  }, [])
-
-  console.log(currentUser)
 
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <NavBar />       
+        <NavBar />
         <Routes>
-          <Route path="/" element={<ItemListContainer setCurrentProduct={setCurrentProduct} greeting={"María Laura"}  filterType= {"subCategory"} filterData= {"oferta"}/>} />
-          <Route path="/productos/zapatillas-hombre" element={<ItemListContainer setCurrentProduct={setCurrentProduct} filterType= {"category"} filterData= {"zapatillas-hombre"}/>} />
-          <Route path="/productos/zapatillas-mujer" element={<ItemListContainer setCurrentProduct={setCurrentProduct} filterType= {"category"} filterData= {"zapatillas-mujer"}/>} />
+          <Route path="/" element={<ItemListContainer setCurrentProduct={setCurrentProduct} greeting={"María Laura"} filterType={"subCategory"} filterData={"oferta"} />} />
+          <Route path="/productos/zapatillas-hombre" element={<ItemListContainer setCurrentProduct={setCurrentProduct} filterType={"category"} filterData={"zapatillas-hombre"} />} />
+          <Route path="/productos/zapatillas-mujer" element={<ItemListContainer setCurrentProduct={setCurrentProduct} filterType={"category"} filterData={"zapatillas-mujer"} />} />
           <Route path="/productos/:productID" element={<ItemDetailContainer {...currentProduct} />} />
-          <Route path="/cart" element={<Cart/>} />          
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
